63a8cd1c1006a7e46c5989a823a81ca9736661ad
rnovick13 <rnovick13@gmail.com>
2020-12-18 16:52:15 -0500

    build create form

--- src/forms/createEmployeeForm.js
+++ src/forms/createEmployeeForm.js
@@ -8,37 +8,48 @@ class createEmployeeForm extends Component {
       first: "",
       last: "",
       salary: "",
-      hire_date: "",
+      hireDate: "",
       position: "",
       manager: ""
     }
   }
 
-<form>
-  <label>
-    Name:
-    <input type="text" name="First"/>
-    <input type="text" name="Last"/>
-  </label>
-  <label>
-    Salary:
-    <input type="text" name="Salary" />
-  </label>
-  <label>
-    Hire Date:
-    <input type="text" name="Hire Date" />
-  </label>
-  <label>
-    Position:
-    <input type="text" name="Position" />
-  </label>
-  <label>
-    Manager:
-    <input type="text" name="Manager" />
-  </label>
-  <input type="submit" value="submit" />
-</form>
+  componentDidMount() {
+    
+  }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    //use a function for a post request
+  }
+render() {
+  return(
+    <form onSubmit={event => this.handleSubmit(event)}>
+      <label>
+        Name:
+        <input type="text" name="First" value={this.state.first}/>
+        <input type="text" name="Last" value={this.state.last}/>
+      </label>
+      <label>
+        Salary:
+        <input type="text" name="Salary" value={this.state.salary}/>
+      </label>
+      <label>
+        Hire Date:
+        <input type="text" name="Hire Date" value={this.state.hireDate}/>
+      </label>
+      <label>
+        Position:
+        <input type="text" name="Position" value={this.state.position}/>
+      </label>
+      <label>
+        Manager:
+        <input type="text" name="Manager" value={this.state.manager}/>
+      </label>
+      <input type="submit" value="submit" />
+    </form>
+    )
+  }
 }
 
 export default createEmployeeForm
