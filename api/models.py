4e27e461f3fcfc432b5677536ceb9874aa43dae4
rnovick13 <rnovick13@gmail.com>
2020-12-18 14:51:22 -0500

    work on models fix typos

--- api/models.py
+++ api/models.py
@@ -1,9 +1,14 @@
 #from {add db} import models
 
 class Employee():
-    first =
-    last =
-    salary =
-    hire_date =
-    position =
-    manager
+    first = db.first_name
+    last = db.last_name
+    salary = db.salary
+    hire_date = db.hire_date
+    position = db.position
+    manager = db.manager
+
+    def __init__(self):
+        pass
+
+#create functions, __init__, update, delete
