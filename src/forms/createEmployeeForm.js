5cc94df2c549e10a56a06fa4c61ebcae29143d93
rnovick13 <rnovick13@gmail.com>
2020-12-18 15:28:12 -0500

    build createEmployeeForm

---
+++ src/forms/createEmployeeForm.js
@@ -0,0 +1,24 @@
+<form>
+  <label>
+    Name:
+    <input type="text" name="First"/>
+    <input type="text" name="Last"/>
+  </label>
+  <label>
+    Salary:
+    <input type="text" name="Salary" />
+  </label>
+  <label>
+    Hire Date:
+    <input type="text" name="Hire Date" />
+  </label>
+  <label>
+    Position:
+    <input type="text" name="Position" />
+  </label>
+  <label>
+    Manager:
+    <input type="text" name="Manager" />
+  </label>
+  <input type="submit" value="submit" />
+</form>
